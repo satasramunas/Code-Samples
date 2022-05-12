553cbd99da55336d22ef7054239051510295e556
Ramunas <ramunas.satas@outlook.com>
2022-05-12 20:02:10 +0300

    added required methods
    added the required methods: Balance(), TopUp(), Buy(). Still not working

--- Console/ShopApp/ShopApp/Services/CustomerService.cs
+++ Console/ShopApp/ShopApp/Services/CustomerService.cs
@@ -15,5 +15,29 @@ namespace ShopApp.Services
         {
             _boughtItems = new List<CustomerItem>();
         }
+
+        public decimal Balance()
+        {
+            CustomerItem item = new CustomerItem();
+            return item.Balance;
+        }
+
+        public void TopUp(decimal balance)
+        {
+            CustomerItem item = new CustomerItem()
+            {
+                Balance = balance += balance
+            };
+        }
+
+        public void Buy(string name, int quantity)
+        {
+
+        }
+
+        public List<CustomerItem> GetBoughtItems()
+        {
+            return _boughtItems;
+        }
     }
 }

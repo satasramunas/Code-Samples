1be0b40cbcb10b102e4ffc750727399a2de4e2ce
Ramunas <ramunas.satas@outlook.com>
2022-05-12 14:44:03 +0300

    Nullable and ShopItem.cs
    Deleted the nullable annotations line and specified ShopItem properties

--- Console/ShopApp/ShopApp/Models/ShopItem.cs
+++ Console/ShopApp/ShopApp/Models/ShopItem.cs
@@ -8,6 +8,8 @@ namespace ShopApp.Models
 {
     public class ShopItem
     {
-
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
     }
 }

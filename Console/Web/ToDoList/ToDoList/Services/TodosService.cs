4320293122944d757b14eced9b23d651fa1465d4
Ramunas <ramunas.satas@outlook.com>
2022-05-16 18:34:35 +0300

    started app with Razor
    initial commit of the app with Razor. Just the very beginning

---
+++ Console/Web/ToDoList/ToDoList/Services/TodosService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Models;
+
+namespace ToDoList.Services
+{
+    public class TodosService
+    {
+        private List<Todo> _todos = new List<Todo>()
+        {
+            new Todo()
+            {
+                Category = "Studying",
+                CreateUtc = System.DateTime.Now,
+                Name = "Study programming"
+            }
+        };
+    }
+}

e7b4c8055e3741578b10c9f85ce1155ba5f5b7bf
Ramunas <ramunas.satas@outlook.com>
2022-05-20 14:54:30 +0300

    connected a DB
    established a connection to a local database

---
+++ Databases/SqlConnectionSolution/SqlConnectionSolution/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+string connectionString;
+SqlConnection cnn;
+connectionString = @"Server=.;Database=TestDataBase;Trusted_Connection=True;";
+cnn = new SqlConnection(connectionString);
+cnn.Open();
+cnn.Close();

06b29a024f4cbe8f5f81e08f480a3ee05eaf88f1
satasramunas <ramunas.satas@outlook.com>
2022-06-07 17:05:19 +0300

    created generics class
    using the <T> for generics

---
+++ Razor/ShopManagementWebApp/ShopManagementWebApp/Services/Base/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using ShopManagementWebApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagementWebApp.Services.Base
+{
+    public class BaseService<T>
+        where T : class
+    {
+        private DataContext _dataContext;
+
+        public BaseService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+    }
+}

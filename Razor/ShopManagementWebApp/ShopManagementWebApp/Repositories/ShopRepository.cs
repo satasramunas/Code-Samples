9a05add2f62520af15835565c4a6e47d89137468
satasramunas <ramunas.satas@outlook.com>
2022-06-08 12:20:19 +0300

    migrated GetAll, Add and Delete to ShopRepository

--- Razor/ShopManagementWebApp/ShopManagementWebApp/Repositories/ShopRepository.cs
+++ Razor/ShopManagementWebApp/ShopManagementWebApp/Repositories/ShopRepository.cs
@@ -1,4 +1,5 @@
 ﻿using ShopManagementWebApp.Data;
+using ShopManagementWebApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,28 @@ namespace ShopManagementWebApp.Repositories
         public ShopRepository(DataContext dataContext)
         {
             _dataContext = dataContext;
+        }
+
+        public List<ShopItem> GetAll()
+        {
+            return _dataContext.Items.ToList();
+        }
+
+        public void Add(ShopItem entity)
+        {
+            entity.ExpiryDate = DateTime.Now;
+            _dataContext.Set<ShopItem>().Add(entity);
+            _dataContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var item = _dataContext.Items.FirstOrDefault(t => t.Id == id);
+            _dataContext.Items.Remove(item);
+            _dataContext.SaveChanges();
+            // these lines instead of the one that's below
+
+            //_items = _items.Where(x => x.Name != name).ToList();
         }
     }
 }

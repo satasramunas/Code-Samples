d6989a2e2f8f5b73853119b3fcab7e98061d4892
satasramunas <ramunas.satas@outlook.com>
2022-06-08 12:12:59 +0300

    created ShopRepository.cs

---
+++ Razor/ShopManagementWebApp/ShopManagementWebApp/Repositories/ShopRepository.cs
@@ -0,0 +1,19 @@
+﻿using ShopManagementWebApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagementWebApp.Repositories
+{
+    public class ShopRepository
+    {
+        private DataContext _dataContext;
+
+        public ShopRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+    }
+}
